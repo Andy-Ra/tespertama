5aba35770f9111bedb3b377ae28b7dc7b610e9f2
ACER <ACER@192.168.56.1>
2021-06-16 16:06:30 +0700

    tes kedua

---
+++ src/prak_alproii_pertemuan8_modul7/Prak_AlproII_Pertemuan8_Modul7.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prak_alproii_pertemuan8_modul7;
+
+/**
+ *
+ * @author ACER
+ */
+public class Prak_AlproII_Pertemuan8_Modul7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HalamanUtama1 halaman = new HalamanUtama1();
+        halaman.setVisible(true);
+    }
+    
+}
